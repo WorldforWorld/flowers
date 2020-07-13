c7beb8c564fe62a3dd5612d6db6ec4cfbcd5f89b
Yuri Kuzmichev <2091god@mail.ru>
2020-07-13 21:20:15 +0500

    feat: create form and validte phone

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.button');
+document.querySelector('input[type=checkbox]').addEventListener('change', function () {
+  if ( this.checked ) {
+    button.style.background = '#EA137A';
+    button.disabled = false;
+  } else {
+    button.style.background = '#DEDFE0';
+    button.disabled = true;
+  }
+});
+$('form').validate();
+$('#phone').mask("+7(999)999-9999", {autoclear: false});
