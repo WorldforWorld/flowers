ca87ae6ff1c747230fec09f27f55dee3c530a6c2
Yuri Kuzmichev <2091god@mail.ru>
2020-07-14 01:02:26 +0500

    feat: add comment in file main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+/* Включение/отключение кнопки отправки формы при определенном состоянии чекбокса */
 const button = document.querySelector('.button');
 document.querySelector('input[type=checkbox]').addEventListener('change', function () {
   if ( this.checked ) {
-    button.style.background = '#EA137A';
     button.disabled = false;
-    button.classList.toggle("button-active");
+    button.style.background = '#EA137A';
+    button.classList.toggle('button-active');
   } else {
-    button.style.background = '#DEDFE0';
     button.disabled = true;
+    button.style.background = '#DEDFE0';
     button.classList.toggle('button-active');
   }
 });
+/* Валидация формы */
 $('form').validate();
 $('#phone').mask("+7(999)999-99-99", {autoclear: false});
