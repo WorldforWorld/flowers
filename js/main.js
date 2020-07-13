ee14bfa6ec909b6828ada915c595bdbfea438e14
Yuri Kuzmichev <2091god@mail.ru>
2020-07-14 00:56:52 +0500

    feat: add check.svg for checkbox  and add transition on hover, active

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ document.querySelector('input[type=checkbox]').addEventListener('change', functi
   if ( this.checked ) {
     button.style.background = '#EA137A';
     button.disabled = false;
+    button.classList.toggle("button-active");
   } else {
     button.style.background = '#DEDFE0';
     button.disabled = true;
+    button.classList.toggle('button-active');
   }
 });
 $('form').validate();
